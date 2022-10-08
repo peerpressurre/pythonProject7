e072f8292a2b45092030e38e81667c1754cc119e
katet0k <katakostucenko0@gmail.com>
2022-10-08 11:20:43 +0300

    Update: feature/lab_ex3

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-
-
-
-
-
+try:
+    sighn = int(input('Enter sigh->'))
+    length = int(input('Enter length->'))
+    print('h = horizontal/n v = vertical')
+    choice = input('->')
+    for x in range(0,length):
+        if choice == 'h':
+            print(sighn)
+        elif choice == 'v':
+            print(sighn\n)
+        else:
+            raise Exception:
+        print('Error: incorrect choice symble')
+except Exception:
+    print
