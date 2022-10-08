592443d0fff820608ef84fdf0d9b58f0b3f56741
katet0k <katakostucenko0@gmail.com>
2022-10-08 11:10:50 +0300

    Update: feature/lab_ex3

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-try:
-    num = int(input('Enter number->'))
-    start = 1
-    sum = 1
 
-    while sum <= num:
-        sum *= start
-        start += 1
-    print(f'{sum}')
-except Exception as ex:
-    print(f'Error information: {ex}')
 
 
 
